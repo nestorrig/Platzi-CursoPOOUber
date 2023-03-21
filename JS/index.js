47dbcd3be495c8e0a5d2c72825d70ef9e1fbcf8d
nestorrg18 <neriga10@hotmail.com>
2023-03-20 19:22:19 -0600

    Clase 23: Declarando un Método Constructor en Java y JavaScript

---
+++ JS/index.js
@@ -0,0 +1,3 @@
+var car = new Car("AW456", new Account("Andres Herrera", "ADP564"));
+car.passenger = 4;
+car.printDataCar();
--- Java/Car.java
+++ Java/Car.java
@@ -3,7 +3,13 @@ class Car {
     String license;
     String driver;
     Integer passegenger;
+
+    public Car(String license, Account driver){
+        this.license = license;
+        this.driver = driver
+    }
+
     void printDataCar() {
-        System.out.println("License: " + license + " Driver: " + driver);
+        System.out.println("License: " + license + " Name Driver: " + driver.name);
     }
 }
